c2034670e5bbb7f3b8a449481b2da3a1d2c836d3
Manuilenkoart <manuilenkoart@gmail.com>
2019-10-22 23:42:29 +0300

    Updates

---
+++ precache-manifest.5a7e0cf1b1f296afcaed9914d35d0777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c1519cb55e4550191c6ca0320dbb08",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "2cf1f6f9f5ce2c368ac9",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "61a6a7e1bcff8a94f585",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.326de1e6.chunk.css"
+  },
+  {
+    "revision": "2cf1f6f9f5ce2c368ac9",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.66e0c80c.chunk.js"
+  },
+  {
+    "revision": "61a6a7e1bcff8a94f585",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.793c157c.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
